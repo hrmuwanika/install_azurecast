0448de77d537fe3c12dc3efeb2ce12490dfa5eba
Henry Robert Muwanika <hrmuwanika@gmail.com>
2023-10-30 07:54:53 +0200

    Update install_azuracast.sh

--- install_azuracast.sh
+++ install_azuracast.sh
@@ -9,11 +9,10 @@ sudo mkdir -p /var/azurecast
 cd /var/azurecast
 
 sudo curl -fsSL <https://raw.githubusercontent.com/AzuraCast/AzuraCast/main/docker.sh> > docker.sh
-
 sudo chmod +x docker.sh
-./docker.sh install-docker
-./docker.sh install-docker-compose
 ./docker.sh install
        
-sudo docker pull azuracast/azuracast
-sudo docker-compose -f docker-compose.installer.yml up -d
+# sudo ./docker.sh update-self
+# sudo ./docker.sh update
+
+# sudo docker-compose up -d
